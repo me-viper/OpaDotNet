f4174af722064ec93a3f59a46e8f1e73a9ef204b
akurochka <andrii.kurochka@zorallabs.com>
2023-06-27 15:25:58 +0300

    doc: #3. Improve public API documentation

--- src/OpaDotNet.Wasm/BuiltinContext.cs
+++ src/OpaDotNet.Wasm/BuiltinContext.cs
@@ -5,7 +5,13 @@ namespace OpaDotNet.Wasm;
 [PublicAPI]
 public record BuiltinContext
 {
+    /// <summary>
+    /// Built-in function name.
+    /// </summary>
     public string FunctionName { get; internal init; } = default!;
 
+    /// <summary>
+    /// Reserved for future use.
+    /// </summary>
     public int OpaContext { get; internal init; }
 }
--- src/OpaDotNet.Wasm/Features/IUpdateDataFeature.cs
+++ src/OpaDotNet.Wasm/Features/IUpdateDataFeature.cs
@@ -2,7 +2,31 @@
 
 public interface IUpdateDataFeature : IOpaEvaluatorFeature
 {
+    /// <summary>
+    /// Updates external data.
+    /// </summary>
+    /// <param name="dataJson">JSON to insert or update</param>
+    /// <param name="path">
+    /// The array with path elements.
+    /// Must be an array value with string keys (eg: <c>["a", "b", "c"]</c>)
+    /// </param>
+    /// <example>
+    /// Base object <c>{"a": {"b": 123 }}</c>, path <c>["a", "x", "y"]</c>,
+    /// and value <c>{"foo": "bar"}</c>
+     
+    /// </example>
     void UpdateDataPath(ReadOnlySpan<char> dataJson, IEnumerable<string> path);
     
+    /// <summary>
+    /// Removes JSON from external data.
+    /// </summary>
+    /// <param name="path">
+    /// The array with path elements.
+    /// Must be an array value with string keys (eg: <c>["a", "b", "c"]</c>)
+    /// </param>
+    /// <example>
+    /// Base object <c>{"a": {"b": 123, "x": {"y": {"foo": "bar" }}}}</c>, path <c>["a", "x"]</c>
+    /// will yield <c>{"a": {"b": 123 }}</c>.
+    /// </example>
     void RemoveDataPath(IEnumerable<string> path);
 }
