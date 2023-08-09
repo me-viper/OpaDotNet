40a0580e1582d4fb3e4d5163fba9b9bbf8f520b6
me-viper <me-viper@users.noreply.github.com>
2023-08-09 07:22:34 +0000

    deploy: 6e8c624bb452900264ddaad97f5aba5d17c3ed3b

--- public/main.js
+++ public/main.js
@@ -1,1 +1,11 @@
-export default {}
+/* file: my-template/public/main.js */
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+      {
+        icon: 'github',
+        href: 'https://github.com/me-viper/OpaDotNet',
+        title: 'GitHub'
+      }
+    ]
+  }
