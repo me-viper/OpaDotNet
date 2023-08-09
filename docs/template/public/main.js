1896b527d176cc0c233477bb6457e7336dada365
akurochka <aki.viper@gmail.com>
2023-08-09 10:03:16 +0300

    ci: Docs CI

---
+++ docs/template/public/main.js
@@ -0,0 +1,11 @@
+/* file: my-template/public/main.js */
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+      {
+        icon: 'github',
+        href: 'https://github.com/me-viper/OpaDotNet',
+        title: 'GitHub'
+      }
+    ]
+  }
