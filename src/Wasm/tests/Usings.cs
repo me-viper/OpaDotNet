33bbc7a2cf514368c0868989c48448a189f2eb57
akurochka <aki.viper@gmail.com>
2024-10-04 16:31:04 +0300

    chore: Optimize dependencies

--- src/Wasm/tests/Usings.cs
+++ src/Wasm/tests/Usings.cs
@@ -3,6 +3,9 @@ global using Xunit.Abstractions;
 
 global using JetBrains.Annotations;
 
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Logging.Abstractions;
+
 #if DISABLEPARALLELIZATION
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
 #endif
