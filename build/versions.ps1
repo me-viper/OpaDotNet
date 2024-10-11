3d020bc21b22f78815103cf59d30b95ae0a38fe5
akurochka <aki.viper@gmail.com>
2024-10-11 10:29:11 +0300

    chore: Improve build scripts

--- build/versions.ps1
+++ build/versions.ps1
@@ -1,2 +1,9 @@
+#! /bin/pwsh
+param (
+    [Parameter(ValueFromRemainingArguments)]
+    [string[]]
+    $Remaining
+)
+
 Get-ChildItem version.json -Recurse `
-    | % { Write-Host "$_" -ForegroundColor Green; dotnet nbgv get-version --project $_.Directory.FullName }
+    | % { Write-Host "$_" -ForegroundColor Green; dotnet nbgv get-version --project $_.Directory.FullName @Remaining }
