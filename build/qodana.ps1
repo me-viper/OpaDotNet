364576288262fff1f97df2054adc39aa675addfa
Andrii.Kurochka <andrii.kurochka@zorallabs.com>
2024-10-11 08:18:22 +0000

    chore: Fix qodana script

---
+++ build/qodana.ps1
@@ -0,0 +1,35 @@
+#! /bin/pwsh
+
+param (
+    [string]
+    $Token,
+    [string]
+    $Image = "jetbrains/qodana-cdnet:latest",
+    [switch]
+    $SkipScan
+)
+
+$ErrorActionPreference = "Stop"
+
+$token = $Token ?? $env:QODANA_TOKEN
+
+$basePath = Resolve-Path "$PSScriptRoot/.."
+$slnPath = "$basePath/qodana.sln"
+
+if (Test-Path $slnPath) {
+    rm $slnPath
+}
+
+dotnet new solution -n qodana -o "$basePath"
+
+Get-ChildItem "$basePath/src" -Include *.csproj -Recurse `
+    | ? { Select-Xml -Path $_.FullName -XPath "//PropertyGroup/IsPackable[.='true']" }
+    | % { dotnet sln $slnPath add $_.FullName --in-root }
+
+if (-not $SkipScan) {
+    docker run -v ${basePath}:/data/project/ -e QODANA_TOKEN="$token" $Image --solution "qodana.sln"
+
+    # if (Test-Path $slnPath) {
+    #     rm $slnPath
+    # }
+}
