db97fe1d4fd1a3cc0d54cd75a97b143b348f7f97
akurochka <aki.viper@gmail.com>
2025-02-11 12:39:00 +0200

    test: Move test cases

---
+++ src/Wasm/tests/SdkV1/TestCaseGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace OpaDotNet.Wasm.Tests.SdkV1;
+
+[Generator]
+public class TestCaseGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(
+            static p =>
+            {
+                //p.AddEmbeddedAttributeDefinition();
+            }
+            );
+    }
+}
